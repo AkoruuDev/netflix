5bdbf71a96ea31f73b006e76edc7cf7e868f18f8
Allan Freitas (Akoruu) <96091754+AkoruuDev@users.noreply.github.com>
2023-07-31 23:23:38 +0000

    feat: create db model

---
+++ database/db.js
@@ -0,0 +1,57 @@
+const films = [
+    {
+        name: "Nome do filme",
+        folder: "https://www.png.com",
+        link: "https://www.movie.com/api/nome-do-filme.mp3",
+        year: "2020",
+        sinopse: "lorem ipsun",
+        trainer: "https://www.movie.com/api/trainer/nome-do-trailer.mp3"
+    }
+]
+
+const genders = [
+    {
+        name: "Genero"
+    }
+]
+
+const directors = [
+    {
+        name: "name"
+    }
+]
+
+const actors = [
+    {
+        name: "Actor"
+    }
+]
+
+const user = [
+    {
+        name: "Nome do usuário",
+        email: "email do usuário",
+        password: "Minha senha muito dificil"
+    }
+]
+
+const profile = [
+    {
+        id_user: 1,
+        picture: "http://www.png.com"
+    }
+]
+
+const favorities = [
+    {
+        user_id: 1,
+        movie_id: 1,
+    }
+]
+
+const note = [
+    {
+        movie_id: 1,
+        note: (1-5)
+    }
+]
